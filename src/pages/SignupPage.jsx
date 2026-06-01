ced8cba69182fca2255f77d8dcf54fca1bb3cfc0
Muhammad Saad Khan <rts.advertisinghub@gmail.com>
2026-06-01 15:46:54 +0500

    Created Signup page .tsx

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="glass rounded-[32px] p-8 text-center text-slate-300">
+        <h1 className="text-2xl mb-4">Sign Up</h1>
+      </div>
+    </div>
+  );
+}
